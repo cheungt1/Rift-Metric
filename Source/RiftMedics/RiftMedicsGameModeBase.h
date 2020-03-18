410a70f99c2b610fec5b04a89702196e265e84a4
Z <43123214+zilong-yang@users.noreply.github.com>
2020-03-18 13:33:34 -0400

    Added the rest of the files

---
+++ Source/RiftMedics/RiftMedicsGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RiftMedicsGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RIFTMEDICS_API ARiftMedicsGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
